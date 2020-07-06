277e242bf2415cb23c8721f4fb1f29d712d51e59
Alex.Kozyura <alex-kzr@github.com>
2020-07-06 22:39:28 +1000

    Add bean @Ingect

--- webapp/src/main/java/com/alexkozyura/tutorial/HelloServlet.java
+++ webapp/src/main/java/com/alexkozyura/tutorial/HelloServlet.java
@@ -1,5 +1,6 @@
 package com.alexkozyura.tutorial;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,19 +12,13 @@ import java.io.IOException;
 @WebServlet(urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
+    @Inject
+    private ExampleBean exampleBean;
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        resp.getWriter().append("Hello from servlet! Request method type : " + req.getMethod());
-
-        HttpSession session = req.getSession();
-
-        if(session.getAttribute("name") == null){
-            session.setAttribute("name", "hello session");
-            resp.getWriter().append(" No session");
-        }else {
-            resp.getWriter().append((String) session.getAttribute("name"));
-        }
-
+        resp.getWriter().append("Login: " + exampleBean.getLogin());
+        resp.getWriter().append("\r\nPassword: " + exampleBean.getPassword());
     }
 }
