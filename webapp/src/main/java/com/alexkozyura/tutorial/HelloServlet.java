61062356db201433488336a9c9f61b3c0defa822
Alex.Kozyura <alex-kzr@github.com>
2020-07-06 15:17:12 +1000

    Add session attribute get & set

--- webapp/src/main/java/com/alexkozyura/tutorial/HelloServlet.java
+++ webapp/src/main/java/com/alexkozyura/tutorial/HelloServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/hello")
@@ -12,6 +13,17 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         resp.getWriter().append("Hello from servlet! Request method type : " + req.getMethod());
+
+        HttpSession session = req.getSession();
+
+        if(session.getAttribute("name") == null){
+            session.setAttribute("name", "hello session");
+            resp.getWriter().append(" No session");
+        }else {
+            resp.getWriter().append((String) session.getAttribute("name"));
+        }
+
     }
 }
